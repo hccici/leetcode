6e5180618a12d098c68dafc0a2a62f6a384e1394
huahongjun <huahongjun1@huawei.com>
2021-06-03 21:00:10 +0800

    huwei

---
+++ source/code/127.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function (beginWord, endWord, wordList) {
+  //  首先把单词去重
+  const set = new Set(wordList)
+  // 因为单词接龙路径是一颗树形结构，所以可以使用bfs去遍历生成路径
+  const queue = [[beginWord, 1]]
+  while (queue.length) {
+    const [curWord, level] = queue.shift()
+    // 查看可以接当前单词的单词都有哪些
+    // 列举可变更的所有单词，如果在单词表中找到，说明它是子节点，推入队列
+    for (let i = 0; i < curWord.length; i++) {
+      for (let j = 97; j <= 122; j++) {
+        const newWord = curWord.slice(0, i) + String.fromCharCode(j) + curWord.slice(i + 1)
+        if (set.has(newWord)) {
+          // 如果找到了就直接返回了，因为是bfs，所以后面能找到的，长度要不相等要不大于
+          if (newWord === endWord) {
+            return level + 1
+          }
+          // 使用过就能去掉了，1是避免重复使用，2是即使通过其它路径也可以转变为这个单词，但是转换长度是一样的，可以不用关心
+          queue.push([newWord, level + 1])
+          set.delete(newWord)
+        }
+      }
+    }
+  }
+  return 0
+};
+console.log(ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]))
---
+++ source/code/934.js
@@ -0,0 +1,73 @@
+/**
+ * @param {number[][]} A
+ * @return {number}
+ */
+var shortestBridge = function (A) {
+  const rows = A.length
+  const cols = A[0].length
+  // 记录当前岛的边缘节点
+  let border = []
+  //  首先找到任一岛的节点，然后进行dfs，找到岛的边缘节点
+  for (let i = 0; i < rows; i++) {
+    if (A[i].includes(1)) {
+      let t = A[i].indexOf(1)
+      dfs(i, t)
+      break
+    }
+  }
+  function dfs(row, col) {
+    if (row < 0 || row >= rows || col < 0 || col >= cols) {
+      return
+    }
+    // 访问过的不再访问
+    if (A[row][col] === 2) {
+      return
+    }
+    // 碰到0就是到了岛的边缘，记录下来并且不再进行访问
+    if (A[row][col] === 0) {
+      A[row][col] = 2
+      border.push([row, col])
+      return
+    }
+    // 访问染色
+    A[row][col] = 2
+    dfs(row + 1, col)
+    dfs(row - 1, col)
+    dfs(row, col + 1)
+    dfs(row, col - 1)
+  }
+  let count = 0
+  let flag = true
+  // 向外扩张并记录扩张后的岛的边缘节点
+  function preVist(row, col) {
+    if (row < 0 || row >= rows || col < 0 || col >= cols || A[row][col] === 3 || A[row][col] === 2) {
+      return false
+    }
+    // 碰到了另一座岛
+    if (A[row][col] === 1) {
+      flag = false
+      return false
+    }
+    A[row][col] = 2
+    return true
+  }
+  function expend(border) {
+    const newBorder = []
+    while (border.length && flag) {
+      let [row, col] = border.pop()
+      preVist(row + 1, col) && newBorder.push([row + 1, col])
+      preVist(row - 1, col) && newBorder.push([row - 1, col])
+      preVist(row, col + 1) && newBorder.push([row, col + 1])
+      preVist(row, col - 1) && newBorder.push([row, col - 1])
+      // 扩张
+      A[row][col] = 3
+    }
+    count++
+    return newBorder
+  }
+  while (flag) {
+    border = expend(border)
+  }
+  return count
+};
+shortestBridge([[0, 1, 0], [0, 0, 0], [0, 0, 1]])
