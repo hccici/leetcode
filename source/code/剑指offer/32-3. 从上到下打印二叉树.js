5a2944f014e311f7cefcad68fd060bd8d56be8f0
huahongjun <huahongjun1@huawei.com>
2021-06-28 19:46:15 +0800

    剑指Offer

---
+++ source/code/剑指offer/32-3. 从上到下打印二叉树.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (!root) {
+    return []
+  }
+  const result = []
+  const stack = [{ node: root, level: 0 }]
+  while (stack.length > 0) {
+    const { node, level } = stack.shift()
+    result[level] || (result.push([]))
+    result[level].push(node.val)
+    node.left && (stack.push({ node: node.left, level: level + 1 }))
+    node.right && (stack.push({ node: node.right, level: level + 1 }))
+  }
+  for (let i = 0; i < result.length; i++) {
+    if (i % 2 === 1) {
+      result[i].reverse()
+    }
+  }
+  return result
+};
