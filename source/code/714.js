259f1a8508c49a272b365aff2bb1b5164291321c
huahongjun <huahongjun1@huawei.com>
2021-08-23 12:23:35 +0800

    动态规划

--- source/code/714.js
+++ source/code/714.js
@@ -1,1 +1,16 @@
-// 动态规划
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function (prices, fee) {
+  // 初始化,index 0：不持有 1：持有
+  const dp = [0, -prices[0]]
+  // 推算
+  for (let i = 1; i < prices.length; i++) {
+    const [last0, last1] = dp
+    dp[0] = Math.max(last0, last1 + prices[i] - fee)
+    dp[1] = Math.max(last1, last0 - prices[i])
+  }
+  return Math.max(dp[0], dp[1])
+};
